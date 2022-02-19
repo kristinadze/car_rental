5dbb77c32adc5021f24a8da1711c0a80a7522ab3
Kristina Dzelebdzic <kristinaar94@gmail.com>
2022-02-19 13:40:28 -0800

    address deleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_222407) do
+ActiveRecord::Schema.define(version: 2022_02_04_174535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 2022_02_03_222407) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.decimal "latitude", precision: 10, scale: 6
+    t.decimal "longitude", precision: 10, scale: 6
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
