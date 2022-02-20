e6bd1534382d330bc0fdc3b465df7503ae778e26
Kristina Dzelebdzic <kristinaar94@gmail.com>
2022-02-20 11:01:17 -0800

    some migrations deleted

--- db/migrate/20220219215427_remove_latitude_from_cars.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLatitudeFromCars < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :cars, :latitude, :decimal
-  end
-end
--- db/migrate/20220219215611_remove_longitude_from_cars.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLongitudeFromCars < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :cars, :longitude, :decimal
-  end
-end
